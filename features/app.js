318df681c7641dcb3ea76105d404970ab6780b22
alejito-code <almelojoder@gmail.com>
2024-08-10 21:52:29 -0500

    descripcion

--- features/app.js
+++ features/app.js
@@ -3,77 +3,92 @@ const peliculas = [
     {
         titulo: "La Guerra de las Galaxias",
         imagen: "/assets/img/anillos.jpeg",
-        descripcion: "La película que revolucionó el cine de ciencia ficción"
+        descripcion: "La película que revolucionó el cine de ciencia ficción",
+        puntuacion: "7/10"
     },
     {
         titulo: "El Padrino",
         imagen: "/assets/img/padrino.jpeg",
-        descripcion: "La película que redefinió el cine de mafia"
+        descripcion: "La película que redefinió el cine de mafia",
+        puntuacion: "7/10"
     },
     {
         titulo: "El Señor de los Anillos",
         imagen: "/assets/img/wars.jpeg",
-        descripcion: "La película que llevó el cine de fantasía a un nuevo nivel"
+        descripcion: "La película que llevó el cine de fantasía a un nuevo nivel",
+        puntuacion: "7/10"       
     },
     {
         titulo: "Avatar",
         imagen: "/assets/img/avatar.jpeg",
-        descripcion: "Una de las peliculas mas taquilleras de la historia"
+        descripcion: "Una de las peliculas mas taquilleras de la historia",
+        puntuacion: "7/10"
     },
     {
         titulo: "Transfomers",
         imagen: "/assets/img/transformers.jpeg",
-        descripcion: "Esta pelicula revoluciono la histroia de las peliculas de ciencia ficcion"
+        descripcion: "Esta pelicula revoluciono la histroia de las peliculas de ciencia ficcion",
+        puntuacion: "7/10"
     },
     {
         titulo: "Armageddon",
         imagen: "/assets/img/armagedon.jpeg",
-        descripcion: "En efecto es cine"
+        descripcion: "En efecto es cine",
+        puntuacion: "7/10"
     },
     {
         titulo: "Interstelar",
         imagen: "/assets/img/interestelar.jpeg",
-        descripcion: "En efecto es aun mas cine"
+        descripcion: "En efecto es aun mas cine",
+        puntuacion: "7/10"
     },
     {
         titulo: "La vida misma",
         imagen: "/assets/img/mismavida.jpeg",
-        descripcion: "En efecto es aun mas cine, si señor"
+        descripcion: "En efecto es aun mas cine, si señor",
+        puntuacion: "7/10"
     },
     {
         titulo: "Avatar",
         imagen: "/assets/img/avatar.jpeg",
-        descripcion: "Una de las peliculas mas taquilleras de la historia"
+        descripcion: "Una de las peliculas mas taquilleras de la historia",
+        puntuacion: "7/10"
     },
     {
         titulo: "Transfomers",
         imagen: "/assets/img/transformers.jpeg",
-        descripcion: "Esta pelicula revoluciono la histroia de las peliculas de ciencia ficcion"
+        descripcion: "Esta pelicula revoluciono la histroia de las peliculas de ciencia ficcion",
+        puntuacion: "7/10"
     },
     {
         titulo: "Armageddon",
         imagen: "/assets/img/armagedon.jpeg",
-        descripcion: "En efecto es cine"
+        descripcion: "En efecto es cine",
+        puntuacion: "7/10"
     },
     {
         titulo: "Interstelar",
         imagen: "/assets/img/interestelar.jpeg",
-        descripcion: "En efecto es aun mas cine"
+        descripcion: "En efecto es aun mas cine",
+        puntuacion: "7/10"
     },
     {
         titulo: "La vida misma",
         imagen: "/assets/img/mismavida.jpeg",
-        descripcion: "En efecto es aun mas cine, si señor"
+        descripcion: "En efecto es aun mas cine, si señor",
+        puntuacion: "7/10"
     },
     {
         titulo: "Interstelar",
         imagen: "/assets/img/interestelar.jpeg",
-        descripcion: "En efecto es aun mas cine"
+        descripcion: "'Interstellar' narra la historia de Joseph Cooper, un granjero que trabajó como astronauta de la NASA, quien debe volver a colocarse su traje de viajero espacial para ir en un viaje casi sin retorno con el fin de salvar a la humanidad de la degradación ambiental que aqueja a la tierra.",
+        puntuacion: "7/10"    
     },
     {
         titulo: "La vida misma",
         imagen: "/assets/img/mismavida.jpeg",
-        descripcion: "En efecto es aun mas cine, si señor"
+        descripcion: "En efecto es aun mas cine, si señor",
+        puntuacion: "7/10"
     }
 ];
 
@@ -90,13 +105,24 @@ peliculas.forEach((pelicula) => {
     const tituloPelicula = document.createElement("h2");
     tituloPelicula.textContent = pelicula.titulo;
 
+    const puntPelicula = document.createElement("h3");
+    puntPelicula.textContent = pelicula.puntuacion;
+
     const descripcionPelicula = document.createElement("p");
     descripcionPelicula.textContent = pelicula.descripcion;
     descripcionPelicula.classList.add("descripcion");
 
+    const infoPelicula = document.createElement("div");
+    infoPelicula.classList.add("info-pelicula");
+    infoPelicula.appendChild(puntPelicula);
+    infoPelicula.appendChild(tituloPelicula);
+    infoPelicula.appendChild(descripcionPelicula);
+
     elementoPelicula.appendChild(imagenPelicula);
     elementoPelicula.appendChild(tituloPelicula);
-    elementoPelicula.appendChild(descripcionPelicula);
+    elementoPelicula.appendChild(infoPelicula);
+    
 
-    contenedorPeliculas.appendChild(elementoPelicula);
+    contenedorPeliculas.appendChild(elementoPelicula); 
 });
+
